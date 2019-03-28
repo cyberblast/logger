b8d79277ba884caa595dacfc6d94320b021cb6f8
cyberblast <git@cyberblast.org>
2019-03-28 18:40:00 +0100

    readme & test

--- readme.md
+++ readme.md
@@ -2,6 +2,37 @@
 
 A simple logger for node
 
-[![GitHub version](https://badge.fury.io/gh/cyberblast%2Flogger.svg)](https://badge.fury.io/gh/cyberblast%2Flogger)
-[![npm version](https://badge.fury.io/js/%40cyberblast%2Flogger.svg)](https://badge.fury.io/js/%40cyberblast%2Flogger)
 [![Build Status](https://travis-ci.com/cyberblast/logger.svg?branch=dev)](https://travis-ci.com/cyberblast/logger)
+[![npm version](https://badge.fury.io/js/%40cyberblast%2Flogger.svg)](https://badge.fury.io/js/%40cyberblast%2Flogger)
+
+## Usage
+
+```js
+// Optional file path argument. Defaults to './logger.json'
+const logger = new Logger('./logger.json');
+// init will create log files and open file streams
+await logger.init();
+// define categories in config file or create them in code (or both)
+logger.defineCategory('Sample');
+// attach additional log handlers
+logger.onLog(log => {
+  console.log(`${log.severity}: ${log.message}`);
+});
+logger.onWarning(log => {
+  console.warn(`Ooops: ${log.message}. Details: ${JSON.stringify(log.data)}`);
+});
+// trigger some log events
+logger.logVerbose('logger ready');
+logger.logWarning('Do the Bartman', logger.category.Sample);
+// close file streams gracefully
+logger.close();
+```
+
+## Configuration
+
+Sorry dude, nothing yet... come back soon to see more.  
+Or [drop me a line](mailto://git@cyberblast.org)...
+
+## Legal
+
+Please take note of files [LICENSE](https://raw.githubusercontent.com/cyberblast/logger/master/LICENSE) and [CONTRIBUTING](https://raw.githubusercontent.com/cyberblast/logger/master/CONTRIBUTING).
--- test/run.js
+++ test/run.js
@@ -1,67 +1,57 @@
 const Logger = require('../src/logger.js');
-const LogFile = require('../src/logfile');
 
 let logger;
-
-//#region Test logfile
-
-const logfile = new LogFile('./test/result/filetest.log');
+let logCount = 0;
+let onLogCount = 0;
 
 function onError(e){
-  if(logfile != null) {
-    console.log(`closing logfile ${logfile.path}`);
-    logfile.close();
-  }
+  console.log('closing logfiles gracefully');
   if(logger != null) logger.close();
+  // throw to break test
   throw e;
 }
 
-try{
-  logfile.writeLine('Hello World!');
-  logfile.writeLine('Here I am.');
-  console.log(`closing logfile ${logfile.path}`);
-  logfile.close();
-
-  //#endregion
-
-  function sleep(ms){
-    return new Promise(resolve=>{
-      setTimeout(resolve,ms)
-    })
-  }
-
-  let count = 0;
-  async function cycle(){
-    do{  
-      await sleep(500);
-      logger.logWarning(`Cycle ${count++}`, logger.category.cat1);
-    }while(count<3);
-    logger.close();
-  }
-
-  function test(){
-    logger.logWarning("Hello World!", logger.category.Test, logger.category);
-    logger.log({
-      category: logger.category.cat1,
-      severity: logger.severity.Info,
-      message: "Into the sun",
-      data: ['yadda', {some: 'thing'}]
-    });
-    
-    cycle().catch(onError);
-  }
-
-  async function run(){
-    logger = new Logger('./test/logger.json');
-    await logger.init();
-    logger.defineCategory('Test');
-    logger.onLog(log => {
-      console.log(JSON.stringify(log));
-    });
-    test();
-  }
-  run();
+function sleep(ms){
+  return new Promise(resolve=>{
+    setTimeout(resolve,ms)
+  })
 }
-catch(e){
-  onError(e);
+
+let count = 0;
+async function cycle(){
+  do{  
+    await sleep(500);
+    logger.logWarning(`Cycle ${count++}`, logger.category.cat1);
+    logCount++;
+  }while(count<3);
+  logger.close();
 }
+
+async function run(){
+  logger = new Logger('./test/logger.json');
+  await logger.init();
+  logger.defineCategory('Test');
+  logger.onLog(log => {
+    console.log(JSON.stringify(log));
+    onLogCount++;
+  });
+  logger.logWarning('Hello World!', logger.category.Test, logger.category);
+  logCount++;
+  logger.log({
+    category: logger.category.cat1,
+    severity: logger.severity.Info,
+    message: 'Into the sun',
+    data: ['yadda', {some: 'thing'}]
+  });
+  logCount++;
+  
+  await cycle().catch(onError);
+}
+
+run()
+  .then(() => {
+    console.log('Checking Results');
+    if(logCount !== onLogCount) throw 'onLog not as often triggered as log!';
+    else console.log('Ok');
+  })
+  .catch(onError);
